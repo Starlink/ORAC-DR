b63228013a38817a7933b50920b750cb1652e9a9
Tim Jenness <t.jenness@jach.hawaii.edu>
1998-10-01 22:56:13 +0000

    Add the -skip support
    svn path=/trunk/; revision=420

--- src/lib/perl5/ORAC/Loop.pm
+++ src/lib/perl5/ORAC/Loop.pm
@@ -8,13 +8,13 @@ ORAC::Loop - data loops for ORACDR
 
   use ORAC::Loop;
  
-  $frm = orac_loop_list($class, $utdate, \@list);
+  $frm = orac_loop_list($class, $utdate, \@list, $skip);
 
   $frm = orac_loop_inf($class, $utdate, \@list);
 
-  $frm = orac_loop_wait($class, $utdate, \@list);
+  $frm = orac_loop_wait($class, $utdate, \@list, $skip);
 
-  $frm = orac_loop_flag($class, $utdate, \@list);
+  $frm = orac_loop_flag($class, $utdate, \@list, $skip);
 
 =head1 DESCRIPTION
 
@@ -30,8 +30,14 @@ It is intended that this routine is called inside an infinite while
 loop with the same @list array. This array is modified by the loop
 routines so that they can keep track of the 'next' frame number.
 
-If a filename can not be found (eg it doesnt exist) undef is 
-returned.
+If a filename can not be found (eg it doesnt exist or the list has
+been processed) undef is returned.
+
+The skip flag is used to indicate whether the loop should skip
+forward if the current observation number can not be found
+but a higher numbered observation is present. Currently no loops
+will go back to missing observations if they appear after a higher
+number (eg observation 10 appears before observation 9!)
 
 =cut
 
@@ -48,35 +54,73 @@ use vars qw/$VERSION @EXPORT @ISA $CONVERT/;
 @ISA = qw/Exporter/;
 @EXPORT = qw/orac_loop_list  orac_loop_wait orac_loop_inf
   orac_loop_flag
+  orac_check_data_dir
   /;
 
 
-=head1 SUBROUTINES
+=head1 LOOP SUBROUTINES
 
 The following loop facilities are available:
 
 =over 4
 
 
-=item orac_loop_list
+=item orac_loop_list(classname, UT, \@array, noskip)
 
 Takes a list of numbers and returns back a frame object 
 for each number (one frame object per call)
 
 undef is returned on error or when all members of the
-list have been returned.
+list have been returned. If the 'skip' flag is true
+missing files in the list will be ignored and the next
+element of the list selected. If 'skip' is false
+the loop will abort if the file is not present 
 
 =cut
 
 sub orac_loop_list {
-  
-  croak 'Wrong number of args: orac_loop_list(class, ut, arr_ref)'
-    unless scalar(@_) == 3; 
 
-  my ($class, $utdate, $obsref) = @_;
+  croak 'Wrong number of args: orac_loop_list(class, ut, arr_ref, skip)'
+    unless scalar(@_) == 4; 
 
-  # Get the obsno
-  my $obsno = shift(@$obsref);
+  my ($class, $utdate, $obsref, $skip) = @_;
+
+  # Initialise variables
+  my $obsno;
+  my $TestFrm = $class->new() if $skip; # Dummy Frame for file check
+
+  # Shift files off the array until we get one that exists
+  # or until we get to the end of the array [ie an undefined value]
+  # If we dont worry about missing observations we immediately
+  # jump out of the loop (ie just read the array once).
+  # If we do care about the existence of the file we have to check
+  # for the input file (-e) and loop round again if it isn't there.
+
+  while (defined ($obsno = shift(@$obsref))) {
+
+    # If we dont care whether the file is there or not (ie default mode)
+    # we jump out of the loop immediately and rely on the link_and_read
+    # routine to abort the main loop for us (by returning undef)
+    last unless $skip; 
+
+    # Ok - we are interested in finding out whether the file is there
+    # or not. There is a slight overhead here in that we have to
+    # do the -e test twice for any successful file. Once here and
+    # once again in link_and_read -- presumably I could have a variant
+    # on link_and_read that gets passed the file name directly
+    # on the understanding that the file is there but for now
+    # I will just do the -e in both places. Note that this relies
+    # on the file being present in ORAC_DATA_IN. It will not look
+    # in ORAC_DATA_OUT first (eg after a FITS conversion)
+    # At some point we need to merge the link_and_read with this
+    # so that this routine has the same robustness as link_and_read.
+
+    my $fname = $TestFrm->file_from_bits($utdate, $obsno);
+
+    last if -e "$ENV{ORAC_DATA_IN}/$fname";
+    orac_warn("Input file $fname not found -- skipping\n");
+
+  }
 
   # If obsno is undef return undef
   return undef unless defined $obsno;
@@ -98,12 +142,18 @@ called.
 undef is returned on error or when there are no more data files
 available.
 
+This loop does not have a facility for skipping files when observations
+are not present. This behaviour is obtained by combining 
+orac_check_data_dir with the list looping option so that the last
+observation number can be determined before running the loop. The skip
+flag is ignored in this loop.
+
 =cut
 
 sub orac_loop_inf {
   
   croak 'Wrong number of args: orac_inf(class, ut, arr_ref)'
-    unless scalar(@_) == 3; 
+    unless (scalar(@_) == 3 || scalar(@_) == 4);
 
   my ($class, $utdate, $obsref) = @_;
 
@@ -139,14 +189,18 @@ new data is expected.
 The loop will return undef (i.e. terminate looping) if the
 supplied array contains undef in the first entry.
 
+The skip flag is used to indicate whether the loop should skip
+forward if the current observation number can not be found
+but a higher numbered observation is present.
+
 =cut
 
 sub orac_loop_wait {
 
-  croak 'Wrong number of args: orac_loop_wait(class, ut, arr_ref)'
-    unless scalar(@_) == 3; 
+  croak 'Wrong number of args: orac_loop_wait(class, ut, arr_ref, skip)'
+    unless scalar(@_) == 4;
 
-  my ($class, $utdate, $obsref) = @_;
+  my ($class, $utdate, $obsref, $skip) = @_;
 
   # If obsno is undef return undef
   return undef unless defined $obsref->[0];
@@ -197,6 +251,50 @@ sub orac_loop_wait {
       last if ($length == $old && $length > 0);
       # store the previous value
       $old = $length;
+
+    } elsif ($skip) {
+
+      # Okay the file was not there -- if SKIP is true we should re-read
+      # the directory to check whether the next file has appeared
+      # In order to prevent the problem of observations n and n+1 appearing
+      # in the time it takes us to read the directory we should make sure
+      # that we are looking for observation n (which has not turned up
+      # yet) rather than n+1. We do this by running orac_check_data_dir
+      # with an observation number one less than we are interested in
+
+      # Run in a scalar context since we are only interested in the next
+      # observation. The flag argument is set to 0.
+      my $next = orac_check_data_dir($class, $obsno - 1, 0);
+
+      # Check to see if something was found
+      if (defined $next) {
+
+	# This indicates that an observation iss available
+	# Now need to modify the name of the file that the loop is 
+	# searching for ($actual) [this is done many times in the loop and 
+	# twice in this routine!]. Do not need to reset the timer since
+	# we already know that we have a file. 
+
+	if ($next != $obsno) {
+	  
+	  orac_print ("\nFile $fname appears to be missing\n");
+
+	  # Okay - it wasnt the expected observation number
+	  $obsno = $next;
+
+	  # Create new filename
+	  $actual = $ENV{ORAC_DATA_IN}."/".
+	    $Frm->file_from_bits($utdate, $next);
+
+	  orac_print("Next available observation is number $obsno");
+
+	  # Loop round
+	  next;
+
+	}
+      
+      }
+
 
     }
 
@@ -251,10 +349,10 @@ supplied array contains undef in the first entry.
 
 sub orac_loop_flag {
 
-  croak 'Wrong number of args: orac_loop_flag(class, ut, arr_ref)'
-    unless scalar(@_) == 3;
+  croak 'Wrong number of args: orac_loop_flag(class, ut, arr_ref, skip)'
+    unless scalar(@_) == 4;
 
-  my ($class, $utdate, $obsref) = @_;
+  my ($class, $utdate, $obsref, $skip) = @_;
 
   # If obsno is undef return undef
   return undef unless defined $obsref->[0];
@@ -284,6 +382,50 @@ sub orac_loop_flag {
   # Dont need to worry about file size
   while (! -e $actual) {
 
+    if ($skip) {
+
+      # Okay the file was not there -- if SKIP is true we should re-read
+      # the directory to check whether the next file has appeared
+      # In order to prevent the problem of observations n and n+1 appearing
+      # in the time it takes us to read the directory we should make sure
+      # that we are looking for observation n (which has not turned up
+      # yet) rather than n+1. We do this by running orac_check_data_dir
+      # with an observation number one less than we are interested in
+
+      # Run in a scalar context since we are only interested in the next
+      # observation. The flag argument is set to true.
+      my $next = orac_check_data_dir($class, $obsno - 1, 1);
+
+      # Check to see if something was found
+      if (defined $next) {
+
+	# This indicates that an observation iss available
+	# Now need to modify the name of the file that the loop is 
+	# searching for ($actual) [this is done many times in the loop and 
+	# twice in this routine!]. Do not need to reset the timer since
+	# we already know that we have a file. 
+
+	if ($next != $obsno) {
+	  
+	  orac_print ("\nFile $fname appears to be missing\n");
+
+	  # Okay - it wasnt the expected observation number
+	  $obsno = $next;
+
+	  # Create new filename
+	  $actual = $ENV{ORAC_DATA_IN}."/".
+	    $Frm->flag_from_bits($utdate, $next);
+
+	  orac_print("Next available observation is number $obsno");
+
+	  # Finish loop since we have found a file
+	  last;
+
+	}
+	
+      }
+    }
+
     # Sleep for a bit
     $timer += sleep($pause);
 
@@ -307,16 +449,157 @@ sub orac_loop_flag {
 
   # Now need to increment obsnum for next time round
   $$obsref[0]++;
-
+  
   return $Frm;
+  
 }
 
+=back
+
+=head1 OTHER EXPORTED SUBROUTINES
+
+=over 4
+
+=item orac_check_data_dir(ClassName, CurrentObsNum, Flag)
+
+Routine to check the input data directory (ORAC_DATA_IN) for
+files in order to see whether files exist with a higher number
+than the supplied number. The routine is supplied with a class name,
+UT date and current observation number. An additional argument
+is provided to determine whether data files or flag files should
+be used for the directory search.
+
+If called in a scalar context, the return argument is the next 
+observation in the sequence. If called in an array context, two
+arguments are returned: the next observation number and the highest 
+observation number.
+
+undef (or undef,undef) is returned if no higher observations can be
+found. If it is necessary to check for the existence of current
+file as well (eg via a data detection loop) then simply decrement the
+supplied argument by 1.
+
+This routine is used in conjunction with the -from loop (where we
+dont know the end) and the waiting loops where we are not sure whether
+new data have been written to disk but missing the next observation.
+
+This routine does NOT look in ORAC_DATA_OUT.
+
+A global variables (@LIST) is used to speed up the sorting by storing
+a list of observation numbers that have previously been shown to have a lower
+number than required (NOT YET IMPLEMENTED).
+
+=cut
+
+sub orac_check_data_dir {
+
+  local (*DATADIR);
+
+  croak 'Usage check_data_dir(ClassName, CurrentObs, Flag)'
+    unless scalar(@_) == 3;
+
+  my ($class, $obsnum, $flag) = @_;
+
+  # Create a new dummy Frame
+  my $DummyFrm = $class->new();
+
+  # Now retrieve the pattern
+  # The most general implementation (and most robust) would
+  # be to query the frame object for a pattern that can be used
+  # to match files.
+  # For now, we will make a guest at the pattern. This could
+  # throw us off the scent in some special cases [eg the directory
+  # contains more than one night of data, DR files which match
+  # the generic pattern....]
+  # If FLAG is true then we simply search for the dot files with 
+  # a .ok at the end 
+
+  my $pattern;
+
+  if ($flag) {
+
+    $pattern = '^\..*\.ok$';   # ' - dummy quote for emacs colour
+  } else {
+
+    # We are matching data files. Try to match a string that
+    # contains the fixed part and ends with the suffix.
+    # (something like UFTI with a single character fixed part
+    # may be tricky). Cant match the UT date since I dont know
+    # where the UT fits into the filename convention
+
+    $pattern = $DummyFrm->rawfixedpart . '.*' . 
+      $DummyFrm->rawsuffix . '$';  # ' dummy quote again
+
+  }
+  
+  # Now open the directory
+  opendir(DATADIR, "$ENV{ORAC_DATA_IN}") 
+    or die "Error opening ORAC_DATA_IN: $!";
+
+  # Read the directory, extract the number and sort the list
+  # Note that I dont even keep the filenames, just the numbers.
+  # Do this with a sort/map/grep combination
+
+  my @sort = sort { $a <=> $b }
+               map { $DummyFrm->raw($_); $DummyFrm->number }
+                 grep { /$pattern/ } readdir(DATADIR);
 
 
-# General subroutine for converting ut and number into file
-# and creating a Frame object
-# undef is returned on error
-# a configured Frame object is returned if everything is okay
+  # Close data directory
+  closedir DATADIR;
+
+  # Now need to compare with the supplied observation number
+  # Go through the sorted list until we find a number that is 
+  # greater than the current observation. Cant simply take the
+  # slice since the index in the sort array is not related to the
+  # observation number
+
+  my $next = undef;   # Next number up
+  foreach (@sort) {
+    if ($_ > $obsnum) {
+      $next = $_;
+      last;
+    }
+  }
+
+  # If $next is defined, that means we have found the next observation
+  # in the list. 
+
+  # If we are in a scalar context we also need the highest number found
+  # The highest value is the last member unless $next
+  # is undef (meaning we got to the end of the list without finding
+  # a higher value)
+
+  if (wantarray) {
+    my $highest = undef;
+    $highest = $sort[-1] if defined $next;
+    return ($next, $highest);
+
+  } else {
+    # We are in a scalar context - just return the next value
+    return $next;
+    
+  }
+
+}
+
+=back
+
+
+=head1 PRIVATE SUBROUTINES
+
+The following subroutines are not exported.
+
+=over 4
+
+=item link_and_read(className, UTdate, ObsNum)
+
+General subroutine for converting ut and number into file
+and creating a Frame object
+undef is returned on error
+a configured Frame object is returned if everything is okay
+
+=cut
 
 sub link_and_read {
 
