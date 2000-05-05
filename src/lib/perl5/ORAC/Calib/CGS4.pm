2ae13960674ef24866f8a5a7bd95631c0abf97f4
Tim Jenness <t.jenness@jach.hawaii.edu>
2000-05-05 05:26:17 +0000

    - Add row index entry - add new constructor
    svn path=/trunk/; revision=1638

--- src/lib/perl5/ORAC/Calib/CGS4.pm
+++ src/lib/perl5/ORAC/Calib/CGS4.pm
@@ -38,15 +38,48 @@ use vars qw/$VERSION/;
 use Carp;
 use strict;
 
+=head1 METHODS
+
+The following methods are available:
+
+=head2 Constructor
+
+=over 4
+
+=item B<new>
+
+Sub-classed constructor. Adds knowledge of extraction rows.
+
+  my $Cal = new ORAC::Calib::CGS4;
+
+=cut
+
+sub new {
+  my $self = shift;
+  my $obj = $self->SUPER::new(@_);
+
+  # Assumes we have a hash object
+  $obj->{RowName}     = undef;
+  $obj->{RowIndex}    = undef;
+
+  return $obj;
+
+}
+
+
+=back
+
+=head2 Accessors
+
 =over 4
 
 =item B<mask>
 
-Return (or set) the name of the rotation transformation matrix
+Return (or set) the name of the bad pixel mask
 
   $mask = $Cal->mask;
 
-For CGS4 this is set to $ORAC_DATA_CAL/bpm by default
+For CGS4 this is set to C<$ORAC_DATA_CAL/fpa46_long> by default
 
 =cut
 
@@ -59,17 +92,103 @@ sub mask {
     $self->{Mask} = $ENV{ORAC_DATA_CAL}."/fpa46_long";
   };
 
-
   return $self->{Mask}; 
 };
 
-=item B<flat>
+=item B<rowname>
 
-Return (or set) the flatfield file.
+Returns the name of the key to use in the index file to retrieve
+the currently accepted positions of the positive and negative row.
+The value should be compared with the current frame header in order
+to guarantee its suitability. The name is usually the name of the
+observation frame used to calculate the row positions.
+
+Can be used to set or retrieve the name.
+
+  $name = $Cal->rowname;
+  $Cal->rowname($name);
 
 =cut
 
+sub rowname {
+  my $self = shift;
+  if (@_) { $self->{RowName} = shift; }
+  return $self->{RowName};
+}
 
+=item B<rowindex>
+
+The ORAC::Index object associated with the extraction row.
+
+=cut
+
+sub rowindex {
+  my $self = shift;
+
+  if (@_) { $self->{RowIndex} = shift; }
+
+  unless (defined $self->{RowIndex}) {
+    my $indexfile = $ENV{ORAC_DATA_OUT}."/index.row";
+    my $rulesfile = $ENV{ORAC_DATA_CAL}."/rules.row";
+    $self->{RowIndex} = new ORAC::Index($indexfile,$rulesfile);
+  };
+
+
+  return $self->{RowIndex}; 
+
+}
+
+=back
+
+=head2 General Methods
+
+=over 4
+
+=item B<rows>
+
+Returns the relevant extraction rows to the caller by comparing
+the index entries with the current frame. Suitable
+values will be found or the method will abort.
+
+Returns two numbers, the position of the positive row and the position
+of the negative.
+
+  my ($posrow, $negrow) = $Cal->rows;
+
+Can not be used to set the name of the index key. Use the C<rowname>
+method for that.
+
+=cut
+
+sub rows {
+  my $self = shift;
+
+  # Compare the current value with the index entry
+  my $rowname = $self->rowname;
+  my $ok = $self->rowindex->verify( $rowname, $self->thing );
+  
+  # If this was not okay we need to search the index
+  unless ($ok) {
+
+    $rowname = $self->rowindex->choosebydt('ORACTIME', $self->thing);
+    croak "No suitable row could be found in index file"
+      unless defined $rowname;
+
+    # Store it
+    $self->rowname( $rowname );
+  }
+
+  # Retrieve the POSROW and NEGROW from the index
+  my $entry = $self->rowindex->indexentry($rowname);
+
+  # Sanity check
+  croak "POSROW could not be found in index entry $rowname\n"
+    unless (exists $entry->{POSROW});
+  croak "NEGROW could not be found in index entry $rowname\n"
+    unless (exists $entry->{NEGROW});
+
+  return ( $entry->{POSROW}, $entry->{NEGROW} );
+}
 
 
 =back
