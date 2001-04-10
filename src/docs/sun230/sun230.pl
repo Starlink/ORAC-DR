6141ca226632406d1b779c5ffec2a3e3aa75e0bc
Tim Jenness <t.jenness@jach.hawaii.edu>
2001-04-10 21:51:49 +0000

    - use strict - tidies up after itself - uses new pod2latex (but hard-wired location)
    svn path=/trunk/; revision=2371

--- src/docs/sun230/sun230.pl
+++ src/docs/sun230/sun230.pl
@@ -1,59 +1,65 @@
 #!/bin/perl
 #
-#
+# Generate SUN/230 from a template and howto files
 #                                                    (frossie@jach.hawaii.edu)
 #------------------------------------------------------------------------
 
+use strict;
 use File::Copy;
 
-$master = "sun230_master.tex";
-$sun = "sun230.tex";
-
-# CHANGE TO YOUR SUN DOCUMENT HERE
-open(MASTER, "<$master");
-open(SUN,">$sun");
+my $master = "sun230_master.tex";
+my $sun = "sun230.tex";
 
 unless (defined $ENV{ORAC_DIR}) {die "Set your ORAC_DIR!\n"};
 unless (defined $ENV{ORAC_INSTRUMENT}) {die "Set your ORAC_INSTRUMENT!\n"};
 
+# CHANGE TO YOUR SUN DOCUMENT HERE
+open(MASTER, "<$master")
+  or die "Unable to open $master for read: $!";
+open(SUN,">$sun")
+  or die "Unable to open $sun for write: $!";
+
 print "Generating $sun from $master\n";
 print "Using pods for $ENV{ORAC_INSTRUMENT} in $ENV{ORAC_DIR} \n";
 
+my $pod = ".pod";
 
-
-foreach $line (<MASTER>) {
+foreach my $line (<MASTER>) {
 
   print SUN $line;
 
   if ($line=~/ORACDRDOC/) {
 
-    ($key,$doc) = split (':',$line,2);
+    my ($key,$doc) = split (':',$line,2);
     chomp($doc);
     
     if ($key =~/HOWTO/) {
 
-      copy ($ENV{ORAC_DIR}."/howto/".$doc,$doc);
+      copy ($ENV{ORAC_DIR}."/howto/".$doc,"$doc$pod");
 
     } elsif ($key=~/PRIMITIVE/) {
 
-      copy ($ENV{ORAC_DIR}."/primitives/".$ENV{ORAC_INSTRUMENT}."/".$doc,$doc);
+      copy ($ENV{ORAC_DIR}."/primitives/".$ENV{ORAC_INSTRUMENT}."/".$doc,"$doc$pod");
 
     } elsif ($key=~/BIN/) {
 
-      copy ($ENV{ORAC_DIR}."/bin/".$doc,$doc);
+      copy ($ENV{ORAC_DIR}."/bin/".$doc,"$doc$pod");
 
     } elsif ($key=~/RECIPE/) {
 
-      copy ($ENV{ORAC_DIR}."/recipes/".$ENV{ORAC_INSTRUMENT}."/".$doc,$doc);
+      copy ($ENV{ORAC_DIR}."/recipes/".$ENV{ORAC_INSTRUMENT}."/".$doc,"$doc$pod");
 
     }
 
-    system("pod2latex $doc");
+    system("/local/perl-5.6/bin/perl /home/timj/perlmods/Pod/LaTeX/pod2latex --modify $doc$pod");
     open (LATEX,"$doc.tex");
 
     print SUN <LATEX>;
 
     print "Done $doc\n";
+
+    unlink "$doc$pod";
+    unlink "$doc.tex";
 
   }
 
