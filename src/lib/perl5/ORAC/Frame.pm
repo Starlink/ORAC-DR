7858f793e45602188a3eb7dd50932b15eef6f981
Brad Cavanagh <b.cavanagh@jach.hawaii.edu>
2002-04-19 19:20:54 +0000

    - Moved formerly UKIRT-specific _generate_orac_lookup_methods method here - Cleaned up some documentation
    svn path=/trunk/; revision=3690

--- src/lib/perl5/ORAC/Frame.pm
+++ src/lib/perl5/ORAC/Frame.pm
@@ -1450,7 +1450,7 @@ sub stripfname {
 
 }
 
-=item B<_split_name>
+=item B<_split_fname>
 
 Given a file name, splits it into an array and a suffix. The first
 array contains the separate components of the file name (in the case
@@ -1481,9 +1481,9 @@ sub _split_fname {
 
 }
 
-=item B<_join_name>
+=item B<_join_fname>
 
-Reverse of C<split_name>.
+Reverse of C<split_fname>.
 
    $file = $frm->_join_fname(\@bits, $suffix);
 
@@ -1502,7 +1502,77 @@ sub _join_fname {
   return $file;
 }
 
+=item B<_from_*>
 
+Methods to translate ORAC_ private headers to FITS headers
+required by the instrument. This is the reverse of C<_to_*> called
+from C<calc_orac_headers>.
+
+These methods should only be called by C<translate_hdr>
+
+Returns a hash containing the FITS key(s) and value(s).
+
+   %fits = $Frm->_from_AIRMASS_START();
+
+The method name does not include the ORAC_ prefix.
+
+=item B<_to_*>
+
+Methods to translate standard FITS headers to ORAC_ headers.
+These methods should be called just from C<orac_calc_headers>.
+
+Returns the translated value.
+
+  $val = $Frm->_to_AIRMASS_START();
+
+The method name does not include the ORAC_ prefix.
+
+=cut
+
+# Generate the methods automatically from a lookup table
+
+# This method generates all the internal methods
+# Expects a hash ref as argument and simply does a name
+# translation without any data processing
+# The hash is keyed by the ORAC_ name (without the ORAC_ prefix
+# (although that will be removed if it appears)
+# This is a class method (no object required)
+sub _generate_orac_lookup_methods {
+  my $class = shift;
+  my $lut = shift;
+
+  # Have to go into a different package
+  my $p = "{\n package $class;\n";
+  my $ep = "\n}"; # close the scope
+
+  # Loop over the keys to the hash
+  for my $key (keys %$lut) {
+
+    # Get the original FITS header name
+    my $fhdr = $lut->{$key};
+
+    # Remove leading ORAC_ if it is there since the method
+    # should not include it
+    $key =~ s/^ORAC_//;
+
+    # prepend ORAC_ for the actual key name
+    my $ohdr = "ORAC_$key";
+
+    # print "Processing $key and $ohdr and $fhdr\n";
+
+    # First generate the code to generate ORAC_ headers
+    my $subname = "_to_$key";
+    my $sub = qq/ $p sub $subname { \$_[0]->hdr(\"$fhdr\"); } $ep /;
+    eval "$sub";
+
+    # Now the from 
+    $subname = "_from_$key";
+    $sub = qq/ $p sub $subname { (\"$fhdr\", \$_[0]->uhdr(\"$ohdr\")); } $ep/;
+    eval "$sub";
+
+  }
+
+}
 
 =back
 
