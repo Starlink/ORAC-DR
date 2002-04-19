b2dc2b4527d064e29c97a6e57495fb38eb88894e
Brad Cavanagh <b.cavanagh@jach.hawaii.edu>
2002-04-19 19:24:03 +0000

    - Removed _generate_orac_lookup_methods method (moved to Frame.pm) - Updated documentation
    svn path=/trunk/; revision=3692

--- src/lib/perl5/ORAC/Frame/UKIRT.pm
+++ src/lib/perl5/ORAC/Frame/UKIRT.pm
@@ -159,92 +159,9 @@ sub findrecipe {
 
 =back
 
-=begin __private
-
-=head1 PRIVATE METHODS
-
-=over 4
-
-=item B<_from_*>
-
-Methods to translate ORAC_ private headers to FITS headers
-required by the instrument. This is the reverse of C<_to_*> called
-from C<calc_orac_headers>.
-
-These methods should only be called by C<translate_hdr>
-
-Returns a hash containing the FITS key(s) and value(s).
-
-   %fits = $Frm->_from_AIRMASS_START();
-
-The method name does not include the ORAC_ prefix.
-
-=item B<_to_*>
-
-Methods to translate standard FITS headers to ORAC_ headers.
-These methods should be called just from C<orac_calc_headers>.
-
-Returns the translated value.
-
-  $val = $Frm->_to_AIRMASS_START();
-
-The method name does not include the ORAC_ prefix.
-
-=cut
-
-# For UKIRT the translation is simple
-# Generate the methods automatically from a lookup table
-
-# This method generates all the internal methods
-# Expects a hash ref as argument and simply does a name
-# translation without any data processing
-# The hash is keyed by the ORAC_ name (without the ORAC_ prefix
-# (although that will be removed if it appears)
-# This is a class method (no object required)
-sub _generate_orac_lookup_methods {
-  my $class = shift;
-  my $lut = shift;
-
-  # Have to go into a different package
-  my $p = "{\n package $class;\n";
-  my $ep = "\n}"; # close the scope
-
-  # Loop over the keys to the hash
-  for my $key (keys %$lut) {
-
-    # Get the original FITS header name
-    my $fhdr = $lut->{$key};
-
-    # Remove leading ORAC_ if it is there since the method
-    # should not include it
-    $key =~ s/^ORAC_//;
-
-    # prepend ORAC_ for the actual key name
-    my $ohdr = "ORAC_$key";
-
-    # print "Processing $key and $ohdr and $fhdr\n";
-
-    # First generate the code to generate ORAC_ headers
-    my $subname = "_to_$key";
-    my $sub = qq/ $p sub $subname { \$_[0]->hdr(\"$fhdr\"); } $ep /;
-    eval "$sub";
-
-    # Now the from 
-    $subname = "_from_$key";
-    $sub = qq/ $p sub $subname { (\"$fhdr\", \$_[0]->uhdr(\"$ohdr\")); } $ep/;
-    eval "$sub";
-
-  }
-
-}
-
-=back
-
-=end __private
-
 =head1 SEE ALSO
 
-L<ORAC::Group>, L<ORAC::Frame::NDF>
+L<ORAC::Group>, L<ORAC::Frame::NDF>, L<ORAC::Frame>
 
 =head1 REVISION
 
