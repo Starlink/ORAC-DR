4ccb79bc81dbeab261cceea055be065020ee95e4
Brad Cavanagh <b.cavanagh@jach.hawaii.edu>
2002-09-10 23:49:43 +0000

    Added emissivity calibration methods
    svn path=/trunk/; revision=3960

--- src/lib/perl5/ORAC/Calib/Michelle.pm
+++ src/lib/perl5/ORAC/Calib/Michelle.pm
@@ -81,6 +81,86 @@ sub skyindex {
 
 =back
 
+=head2 Accessor Methods
+
+=over 4
+
+=item B<emisname>
+
+Return (or set) the name of the current emissivity frame - no checking
+
+  $emis = $Cal->emisname;
+
+=cut
+
+sub emisname {
+  my $self = shift;
+  if( @_ ) { $self->{Emissivity} = shift unless $self->emisnoupdate; }
+  return $self->{Emissivity};
+}
+
+=item B<emis>
+
+Return (or set) the name of the curent emissivity frame - checks
+suitability on return.
+
+=cut
+
+sub emis {
+  my $self = shift;
+  if( @_ ) {
+    return $self->emisname( shift );
+  };
+
+  my $ok = $self->emisindex->verify( $self->emisname, $self->thing );
+
+  if( $ok ) { return $self->emisname };
+
+  croak("Override emissivity frame is not suitable. Giving up") if $self->emisnoupdate;
+
+  if( defined $ok ) {
+    my $emis = $self->emisindex->choosebydt( 'ORACTIME', $self->thing );
+    croak "No suitable emissivity calibration was found in the index file"
+      unless defined $emis;
+    $self->emisname( $emis );
+  } else {
+    croak("Error in emissivity calibration checking - giving up");
+  }
+}
+
+=item B<emisnoupdate>
+
+Stops emissivity calibration object from updating itself with more
+recent data. Used when using a command-line override to the pipeline.
+
+=cut
+
+sub emisnoupdate {
+  my $self = shift;
+  if( @_ ) { $self->{EmissivityNoUpdate} = shift; }
+  return $self->{EmissivityNoUpdate};
+}
+
+=item B<emisindex>
+
+Return (or set) the index object associated with the emissivity index file.
+
+=cut
+
+sub emisindex {
+  my $self = shift;
+  if( @_ ) { $self->{EmissivityIndex} = shift; }
+
+  unless( defined $self->{EmissivityIndex} ) {
+    my $indexfile = $ENV{ORAC_DATA_OUT} . "/index.emis";
+    my $rulesfile = $ENV{ORAC_DATA_CAL} . "/rules.emis";
+    $self->{EmissivityIndex} = new ORAC::Index( $indexfile, $rulesfile );
+  }
+
+  return $self->{EmissivityIndex};
+}
+
+=back
 
 =head2 General Methods
 
