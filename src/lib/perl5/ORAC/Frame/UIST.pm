8e3180923c66ff1fe5f35e116063a83d7a5667d4
Paul Hirst <p.hirst@jach.hawaii.edu>
2002-10-17 07:12:47 +0000

    CDELT1 should have the correct sign aswell as numeric value
    svn path=/trunk/; revision=4113

--- src/lib/perl5/ORAC/Frame/UIST.pm
+++ src/lib/perl5/ORAC/Frame/UIST.pm
@@ -54,7 +54,7 @@ my %hdr = (
 
 # then the spectroscopy...
             CONFIGURATION_INDEX  => "CNFINDEX",
-            GRATING_DISPERSION   => "DISPERSN",
+            GRATING_DISPERSION   => "CDELT1",
             GRATING_NAME         => "GRISM",
             GRATING_ORDER        => "GRATORD",
             GRATING_WAVELENGTH   => "CENWAVL",
