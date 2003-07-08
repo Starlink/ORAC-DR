dc504a31eb5548d503b7035730bd1d8936d8fa6a
Malcolm J. Currie <mjc@star.rl.ac.uk>
2003-07-08 13:26:40 +0000

    Sub-class from ORAC::Group::ESO.  Use more defaults.
    svn path=/trunk/; revision=4837

--- src/lib/perl5/ORAC/Group/ISAAC.pm
+++ src/lib/perl5/ORAC/Group/ISAAC.pm
@@ -16,7 +16,7 @@ ORAC::Group::ISAAC - ISAAC class for dealing with observation groups in ORAC-DR
 =head1 DESCRIPTION
 
 This module provides methods for handling group objects that
-are specific to ISAAC. It provides a class derived from B<ORAC::Group::NDF>.
+are specific to ISAAC. It provides a class derived from B<ORAC::Group::ESO>.
 All the methods available to B<ORAC::Group> objects are available
 to B<ORAC::Group::ISAAC> objects. 
 
@@ -35,67 +35,16 @@ use ORAC::Print;
 use ORAC::General;
 
 # Set inheritance
-use base qw/ORAC::Group::UKIRT/;
+use base qw/ORAC::Group::ESO/;
 
 use vars qw/$VERSION/;
 
 '$Revision$ ' =~ /.*:\s(.*)\s\$/ && ($VERSION = $1);
 
-# Translation tables for ISAAC should go here.
-# First the imaging...
-my %hdr = (
-            DEC_SCALE            => "CDELT1",
-            RA_SCALE             => "CDELT2",
+# Instrument-specific translations.
+# =================================
 
-# then the spectroscopy...
-            CONFIGURATION_INDEX  => "HIERARCH.ESO.INS.GRAT.ENC",
-            GRATING_NAME         => "HIERARCH.ESO.INS.GRAT.NAME",
-            GRATING_ORDER        => "HIERARCH.ESO.INS.GRAT.ORDER",
-            GRATING_WAVELENGTH   => "HIERARCH.ESO.INS.GRAT.WLEN",
-            SLIT_NAME            => "HIERARCH.ESO.INS.OPTI1.ID",
-#            SLIT_NAME            => "HIERARCH.ESO.INS.SLIT",
-            X_DIM                => "HIERARCH.ESO.DET.WIN.NX",
-            Y_DIM                => "HIERARCH.ESO.DET.WIN.NY",
-
-# then the general.
-            AIRMASS_END          => "HIERARCH.ESO.TEL.AIRM.END",
-            CHOP_ANGLE           => "HIERARCH.ESO.SEQ.CHOP.POSANGLE",
-            CHOP_THROW           => "HIERARCH.ESO.SEQ.CHOP.THROW",
-            EXPOSURE_TIME        => "EXPTIME",
-            NUMBER_OF_EXPOSURES  => "HIERARCH.ESO.DET.NDIT",
-            NUMBER_OF_READS      => "HIERARCH.ESO.DET.NCORRS",
-            OBSERVATION_NUMBER   => "OBSNUM",
-	  );
-
-# Take this lookup table and generate methods that can be sub-classed by
-# other instruments.  Have to use the inherited version so that the new
-# subs appear in this class.
-ORAC::Group::ISAAC->_generate_orac_lookup_methods( \%hdr );
-
-sub _to_AIRMASS_START {
-   my $self = shift;
-   my $start_airmass = 1.0;
-   if ( exists $self->hdr->{"HIERARCH.ESO.TEL.AIRM.START"} ) {
-      $start_airmass =  $self->hdr->{"HIERARCH.ESO.TEL.AIRM.START"};
-   }
-   return $start_airmass;
-}
-
-sub _from_AIRMASS_START {
-   "HIERARCH.ESO.TEL.AIRM.START", $_[0]->uhdr( "ORAC_AIRMASS_START" );
-}
-
-sub _to_DEC_BASE {
-   my $self = shift;
-   my $dec = 0.0;
-   if ( exists ( $self->hdr->{DEC} ) ) {
-      $dec = $self->hdr->{DEC};
-   }
-   $dec = defined( $dec ) ? $dec: 0.0; 
-   return $dec;
-}
-
-# If the telescope offset exists in arcsec, then use it.  Otherwise
+# If the telescope ofset exists in arcsec, then use it.  Otherwise
 # convert the Cartesian offsets to equatorial offsets.
 sub _to_DEC_TELESCOPE_OFFSET {
    my $self = shift;
@@ -138,87 +87,8 @@ sub _to_DEC_TELESCOPE_OFFSET {
    }
               
 # The sense is reversed compared with UKIRT, as these measure the
-# place son the sky, not the motion of the telescope.
+# places on the sky, not the motion of the telescope.
    return -1.0 * $decoffset;
-}
-
-# If the telescope offset exists in arcsec, then use it.  Otherwise
-# convert the Cartesian offsets to equatorial offsets.
-sub _to_RA_TELESCOPE_OFFSET {
-   my $self = shift;
-   my $raoffset = 0.0;
-   if ( exists $self->hdr->{"HIERARCH.ESO.SEQ.CUMOFFSETA"} ) {
-      $raoffset = $self->hdr->{"HIERARCH.ESO.SEQ.CUMOFFSETA"};
-
-   } elsif ( exists $self->hdr->{"HIERARCH.ESO.SEQ.CUMOFFSETX"} &&
-             exists $self->hdr->{"HIERARCH.ESO.SEQ.CUMOFFSETY"} ) {
-
-      my $pixscale = 0.148;
-      if ( exists $self->hdr->{"HIERARCH.ESO.INS.PIXSCALE"} ) {
-         $pixscale = $self->hdr->{"HIERARCH.ESO.INS.PIXSCALE"};
-      }
-
-# Sometimes the first cumulative offsets are non-zero contrary to the
-# documentation.
-      my $expno = 1;
-      if ( exists $self->hdr->{"HIERARCH.ESO.TPL.EXPNO"} ) {
-         $expno = $self->hdr->{"HIERARCH.ESO.TPL.EXPNO"};
-      }
-      my ( $x_as, $y_as );
-      if ( $expno == 1 ) {
-         $x_as = 0.0;
-         $y_as = 0.0;
-      } else {
-         $x_as = $self->hdr->{"HIERARCH.ESO.SEQ.CUMOFFSETX"} * $pixscale;
-         $y_as = $self->hdr->{"HIERARCH.ESO.SEQ.CUMOFFSETY"} * $pixscale;
-      }
-
-# Define degrees to radians conversion and obtain the rotation angle.
-      my $dtor = atan2( 1, 1 ) / 45.0;
-
-      my $rotangle = $self->rotation();
-      my $cosrot = cos( $rotangle * $dtor );
-      my $sinrot = sin( $rotangle * $dtor );
-
-# Apply the rotation matrix to obtain the equatorial pixel offset.
-      $raoffset = -$x_as * $cosrot + $y_as * $sinrot;
-   }
-              
-# The sense is reversed compared with UKIRT, as these measure the
-# place on the sky, not the motion of the telescope.
-   return -1.0 * $raoffset;
-}
-
-# This is guesswork at present.
-sub _to_DETECTOR_READ_TYPE {
-   my $self = shift;
-   my $read_type;
-   my $chop = $self->hdr->{"HIERARCH.ESO.TEL.CHOP.ST"};
-   my $readout_mode = $self->hdr->{"HIERARCH.ESO.DET.MODE.NAME"};
-   if ( $readout_mode =~ /Uncorr/ ) {
-      if ( $chop ) {
-         $read_type = "CHOP";
-      } else {
-         $read_type = "STARE";
-      }
-   } else {
-      if ( $chop ) {
-         $read_type = "NDCHOP";
-      } else {
-         $read_type = "NDSTARE";
-      }
-   }
-   return $read_type;
-}
-
-# Equinox may be absent for calibrations such as darks.
-sub _to_EQUINOX {
-   my $self = shift;
-   my $equinox = 0;
-   if ( exists $self->hdr->{EQUINOX} ) {
-      $equinox = $self->hdr->{EQUINOX};
-   }
-   return $equinox;
 }
 
 # Filter positions 1 and 2 used for SW and 3 & 4 for LW.
@@ -315,65 +185,51 @@ sub _to_GRATING_DISPERSION {
    return $dispersion;
 }     
 
-# Sampling is always 1x1, and therefore there are no headers with
-# these values.
-sub _to_NSCAN_POSITIONS {
-   1;
-}
-
-sub _from_NSCAN_POSITIONS {
-   "DETNINCR", 1;
-}
-
-sub _to_NUMBER_OF_OFFSETS {
+# If the telescope offset exists in arcsec, then use it.  Otherwise
+# convert the Cartesian offsets to equatorial offsets.
+sub _to_RA_TELESCOPE_OFFSET {
    my $self = shift;
-   return $self->hdr->{"HIERARCH.ESO.TPL.NEXP"} + 1;
-}
-            
-sub _from_NUMBER_OF_OFFSETS {
-   "HIERARCH.ESO.TPL.NEXP",  $_[0]->uhdr( "ORAC_NUMBER_OF_OFFSETS" ) - 1;
-#   "NEXP",  $_[0]->uhdr( "ORAC_NUMBER_OF_OFFSETS" ) - 1;
-}
+   my $raoffset = 0.0;
+   if ( exists $self->hdr->{"HIERARCH.ESO.SEQ.CUMOFFSETA"} ) {
+      $raoffset = $self->hdr->{"HIERARCH.ESO.SEQ.CUMOFFSETA"};
 
-sub _to_OBSERVATION_MODE {
-   my $self = shift;
-   my $mode = $self->hdr->{"HIERARCH.ESO.DPR.TECH"};
-   if ( uc( $mode ) eq "IMAGE" ) {
-      $mode = "imaging";
-   } else {
-      $mode = "spectroscopy";
+   } elsif ( exists $self->hdr->{"HIERARCH.ESO.SEQ.CUMOFFSETX"} &&
+             exists $self->hdr->{"HIERARCH.ESO.SEQ.CUMOFFSETY"} ) {
+
+      my $pixscale = 0.148;
+      if ( exists $self->hdr->{"HIERARCH.ESO.INS.PIXSCALE"} ) {
+         $pixscale = $self->hdr->{"HIERARCH.ESO.INS.PIXSCALE"};
+      }
+
+# Sometimes the first cumulative offsets are non-zero contrary to the
+# documentation.
+      my $expno = 1;
+      if ( exists $self->hdr->{"HIERARCH.ESO.TPL.EXPNO"} ) {
+         $expno = $self->hdr->{"HIERARCH.ESO.TPL.EXPNO"};
+      }
+      my ( $x_as, $y_as );
+      if ( $expno == 1 ) {
+         $x_as = 0.0;
+         $y_as = 0.0;
+      } else {
+         $x_as = $self->hdr->{"HIERARCH.ESO.SEQ.CUMOFFSETX"} * $pixscale;
+         $y_as = $self->hdr->{"HIERARCH.ESO.SEQ.CUMOFFSETY"} * $pixscale;
+      }
+
+# Define degrees to radians conversion and obtain the rotation angle.
+      my $dtor = atan2( 1, 1 ) / 45.0;
+
+      my $rotangle = $self->rotation();
+      my $cosrot = cos( $rotangle * $dtor );
+      my $sinrot = sin( $rotangle * $dtor );
+
+# Apply the rotation matrix to obtain the equatorial pixel offset.
+      $raoffset = -$x_as * $cosrot + $y_as * $sinrot;
    }
-   return $mode;
-}
-
-sub _from_OBSERVATION_MODE {
-   "HIERARCH.ESO.DPR.TECH",  $_[0]->uhdr( "ORAC_OBSERVATION_MODE" );
-#   "DPRTECH",  $_[0]->uhdr( "ORAC_OBSERVATION_MODE" );
-}
-
-sub _to_OBSERVATION_TYPE {
-   my $self = shift;
-   my $type = $self->hdr->{"HIERARCH.ESO.DPR.TYPE"};
-   if ( uc( $type ) eq "STD" ) {
-      $type = "OBJECT";
-   } elsif ( uc( $type ) eq "SKY,FLAT" ) {
-      $type = "SKY";
-   } elsif ( uc( $type ) eq "LAMP" ) {
-      $type = "ARC";
-   }
-   return $type;
-}
-
-sub _from_OBSERVATION_TYPE {
-   "HIERARCH.ESO.DPR.TYPE",  $_[0]->uhdr( "ORAC_OBSERVATION_TYPE" );
-#   "DPRTYPE",  $_[0]->uhdr( "ORAC_OBSERVATION_TYPE" );
-}
-
-sub _to_RA_BASE {
-   my $self = shift;
-   my $ra = $self->hdr->{RA};
-   $ra = defined( $ra ) ? $ra: 0.0; 
-   return $ra / 15.0;
+              
+# The sense is reversed compared with UKIRT, as these measure the
+# place on the sky, not the motion of the telescope.
+   return -1.0 * $raoffset;
 }
 
 # Derive the translation between observing template and recipe name.
@@ -456,32 +312,6 @@ sub _to_RECIPE {
    return $recipe;
 }
 
-sub _to_ROTATION {
-   my $self = shift;
-   return $self->rotation();
-}
-
-sub _to_SCAN_INCREMENT {
-   1;
-}
-
-sub _from_SCAN_INCREMENT {
-   "DETINCR", 1;
-}
-
-sub _to_SLIT_ANGLE {
-   my $self = shift;
-   my $slitangle = 0.0;
-   if ( exists $self->hdr->{"HIERARCH.ESO.ADA.POSANG"} ) {
-      $slitangle =  $self->hdr->{"HIERARCH.ESO.ADA.POSANG"};
-   }
-   return $slitangle;
-}
-
-sub _from_SLIT_ANGLE {
-   "HIERARCH.ESO.ADA.POSANG",  $_[0]->uhdr( "ORAC_SLIT_ANGLE" );
-}
-
 # Fixed values for the gain depend on the camera (SW or LW), and for LW
 # the readout mode.
 sub _to_SPEED_GAIN {
@@ -501,20 +331,6 @@ sub _to_SPEED_GAIN {
    return $spd_gain;
 }
 
-sub _to_STANDARD {
-   my $self = shift;
-   my $standard = 0;
-   my $type = $self->hdr->{"HIERARCH.ESO.DPR.TYPE"};
-   if ( uc( $type ) eq "STD" ) {
-      $standard = 1;
-   }
-   return $standard;
-}
-
-sub _from_STANDARD {
-   "STANDARD",  $_[0]->uhdr( "ORAC_STANDARD" );
-}
-
 # Translate to the SLALIB name for reference frame in spectroscopy.
 sub _to_TELESCOPE {
    my $self = shift;
@@ -529,155 +345,6 @@ sub _to_TELESCOPE {
    }
    return $telescope;
 }
-
-sub _to_UTDATE {
-   my $self = shift;
-
-# This is UT start and time.
-   my $dateobs = $self->hdr->{"DATE-OBS"};
-
-# Extract out the data in yyyymmdd format.
-   return substr( $dateobs, 0, 4 ) . substr( $dateobs, 5, 2 ) . substr( $dateobs, 8, 2 )
-}
-
-sub _to_UTEND {
-   my $self = shift;
-
-# Obtain the start time in seconds.
-   my $startsec = 3600.0 * $self->get_UT_hours();
-
-# This is approximate end UT in seconds.
-   my $endsec = $startsec + $self->hdr->{EXPTIME};
-
-# Convert from seconds to decimal hours.
-   return $endsec / 3600.0;
-}
-
-sub _to_UTSTART {
-   my $self = shift;
-
-# Obtain the start time in seconds.
-   return $self->get_UT_hours();
-}
-
-sub _to_WAVEPLATE_ANGLE {
-    my $self = shift;
-    my $polangle = 0.0;
-    if ( exists $self->hdr->{"HIERARCH.ESO.SEQ.ROT.OFFANGLE"} ) {
-       $polangle = $self->hdr->{"HIERARCH.ESO.SEQ.ROT.OFFANGLE"};
-    } elsif ( exists $self->hdr->{CROTA1} ) {
-       $polangle = abs( $self->hdr->{CROTA1} );
-    }
-    return $polangle;
-}
-
-# Use the nominal reference pixel if correctly supplied, failing that
-# take the average of the bounds, and if these headers are also absent,
-# use a default which assumes the full array.
-sub _to_X_REFERENCE_PIXEL{
-   my $self = shift;
-   my $xref;
-   if ( exists $self->hdr->{CRPIX1} ) {
-      $xref = $self->hdr->{CRPIX1};
-   } elsif ( exists $self->hdr->{"HIERARCH.ESO.DET.WIN.STARTX"} && exists $self->hdr->{"HIERARCH.ESO.DET.WIN.NX"} ) {
-      my $xl = $self->hdr->{"HIERARCH.ESO.DET.WIN.STARTX"};
-      my $xu = $self->hdr->{"HIERARCH.ESO.DET.WIN.NX"};
-      $xref = nint( ( $xl + $xu ) / 2 );
-   } else {
-      $xref = 504;
-   }
-   return $xref;
-}
-
-sub _from_X_REFERENCE_PIXEL {
-   "CRPIX1", $_[0]->uhdr("ORAC_X_REFERENCE_PIXEL");
-}
-
-# Use the nominal reference pixel if correctly supplied, failing that
-# take the average of the bounds, and if these headers are also absent,
-# use a default which assumes the full array.
-sub _to_Y_REFERENCE_PIXEL{
-   my $self = shift;
-   my $yref;
-   if ( exists $self->hdr->{CRPIX2} ) {
-      $yref = $self->hdr->{CRPIX2};
-   } elsif ( exists $self->hdr->{"HIERARCH.ESO.DET.WIN.STARTY"} && exists $self->hdr->{"HIERARCH.ESO.DET.WIN.NY"} ) {
-      my $yl = $self->hdr->{"HIERARCH.ESO.DET.WIN.STARTY"};
-      my $yu = $self->hdr->{"HIERARCH.ESO.DET.WIN.NY"};
-      $yref = nint( ( $yl + $yu ) / 2 );
-   } else {
-      $yref = 491;
-   }
-   return $yref;
-}
-
-sub _from_Y_REFERENCE_PIXEL {
-   "CRPIX2", $_[0]->uhdr("ORAC_Y_REFERENCE_PIXEL");
-}
-
-sub _to_X_LOWER_BOUND {
-   my $self = shift;
-   return nint( $self->hdr->{"HIERARCH.ESO.DET.WIN.STARTX"} );
-}
-
-sub _to_Y_LOWER_BOUND {
-   my $self = shift;
-   return nint( $self->hdr->{"HIERARCH.ESO.DET.WIN.STARTY"} );
-}
-
-sub _to_X_UPPER_BOUND {
-   my $self = shift;
-   return $self->hdr->{"HIERARCH.ESO.DET.WIN.STARTX"} - 1 + $self->hdr->{"HIERARCH.ESO.DET.WIN.NX"};
-}
-
-sub _to_Y_UPPER_BOUND {
-   my $self = shift;
-   return $self->hdr->{"HIERARCH.ESO.DET.WIN.STARTY"} - 1 + $self->hdr->{"HIERARCH.ESO.DET.WIN.NY"};
-}
-
-
-# Supplementary methods for the translations
-# ------------------------------------------
-
-# Returns the UT time of observation in decimal hours.
-sub get_UT_hours {
-   my $self = shift;
-
-# This is approximate.  UTC is time in seconds.
-   my $startsec = 0.0;
-   if ( exists ( $self->hdr->{UTC} ) ) {
-      $startsec  = $self->hdr->{UTC};
-
-# Use the backup of the observation start header, which is encoded in
-# FITS data format, i.e. yyyy-mm-ddThh:mm:ss.  So convert ot seconds.
-   } elsif ( exists( $self->hdr->{"HIERARCH.ESO.OBS.START"} ) ) {
-      my $t = $self->hdr->{"HIERARCH.ESO.OBS.START"};
-      $startsec = substr( $t, 11, 2 ) * 3600.0 +
-                  substr( $t, 14, 2 ) * 60.0 + substr( $t, 17, 2  );
-   }
-
-# Convert from seconds to decimal hours.
-   return $startsec / 3600.0;
-}
-
-# Derives the rotation angle from the rotation matrix.
-sub rotation{
-   my $self = shift;
-   my $rotangle;
-
-# Define degrees to radians conversion.
-   my $dtor = atan2( 1, 1 ) / 45.0;
-
-   if ( exists $self->hdr->{PC001001} ) {
-      my $pc11 = $self->hdr->{PC001001};
-      my $pc21 = $self->hdr->{PC002001};
-      $rotangle = $dtor * atan2( -$pc21 / $dtor, $pc11 / $dtor );
-   } else {
-      $rotangle = 180.0;
-   }
-   return $rotangle;
-}
-
 
 =head1 PUBLIC METHODS
 
@@ -730,15 +397,6 @@ sub new {
 This method calculates header values that are required by the
 pipeline by using values stored in the header.
 
-Required ORAC extensions are:
-
-ORACTIME: should be set to a decimal time that can be used for
-comparing the relative start times of frames.  For UKIRT this
-number is decimal hours, for SCUBA this number is decimal
-UT days.
-
-ORACUT: This is the UT day of the frame in YYYYMMDD format.
-
 This method should be run after a header is set.  Currently the readhdr()
 method calls this whenever it is updated.
 
@@ -753,27 +411,6 @@ sub calc_orac_headers {
 # Run the base class first since that does the ORAC
 # headers.
    my %new = $self->SUPER::calc_orac_headers;
-
-
-# ORACTIME
-# --------
-# For ISAAC this is the UTC header value converted to decimal hours
-# and a 12-hour offset to avoid worrying about midnight UT.
-   my $time = $self->get_UT_hours() + 12.0;
-
-# Just return it (zero if not available)
-   $time = 0 unless (defined $time);
-   $self->hdr('ORACTIME', $time);
-
-   $new{'ORACTIME'} = $time;
-
-# ORACUT
-# ------
-# For ISAAC this is the UTC header value converted to decimal hours.
-   my $ut =  $self->get_UT_hours();
-   $ut = 0 unless defined $ut;
-   $self->hdr('ORACUT', $ut);
-
    return %new;
 }
 
