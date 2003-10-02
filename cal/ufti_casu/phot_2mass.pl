1b39a9d1a8f9c86a5392a6dcd23183d6fe235153
Jim Lewis <jrl@ast.cam.ac.uk>
2003-10-02 02:45:53 +0000

    Added reference to fs_izjhklm.fit for data not covered by 2mass colours
    svn path=/trunk/; revision=5036

--- cal/ufti_casu/phot_2mass.pl
+++ cal/ufti_casu/phot_2mass.pl
@@ -16,6 +16,30 @@
                          'refmag' => '$Kmag;',
                          'extinct' => '0.05*($airmass - 1.0);',
                          'zeropt' => 24.0,
+                     },
+                     'Z' => {
+                         'catalogue' => 'fs',
+                         'refmag' => '$Zmag;',
+                         'extinct' => '0.05*($airmass - 1.0);',
+                         'zeropt' => 24.0,
+                     },
+                     'I' => {
+                         'catalogue' => 'fs',
+                         'refmag' => '$Imag;',
+                         'extinct' => '0.05*($airmass - 1.0);',
+                         'zeropt' => 24.0,
+                     },
+                     'L98' => {
+                         'catalogue' => 'fs',
+                         'refmag' => '$Lmag;',
+                         'extinct' => '0.05*($airmass - 1.0);',
+                         'zeropt' => 24.0,
+                     },
+                     'M98' => {
+                         'catalogue' => 'fs',
+                         'refmag' => '$Mmag;',
+                         'extinct' => '0.05*($airmass - 1.0);',
+                         'zeropt' => 24.0,
                      }
                     );
  
@@ -26,6 +50,13 @@
                        'columns' => ['Jmag','Hmag','Kmag'],
                        'vars' => ['Jmag','Hmag','Kmag'],
                        'vizcat' => '2mass'
-                       }
+                       },
+                   'fs' => {
+                       'location' => "$ENV{ORAC_DATA_CAL}/fs_izjhklm.fit",
+                       'accessmethod' => "searchfits",
+                       'columns' => ['I','Z','J(98)','H(98)','K(98)','L(98)','M(98)'],
+                       'vars' => ['Imag','Zmag','Jmag','Hmag','Kmag','Lmag','Mmag'],
+                       'vizcat' => ''
+		       }
                   );
 
--- cal/ufti_casu/phot_mycat.pl
+++ cal/ufti_casu/phot_mycat.pl
@@ -16,6 +16,36 @@
                          'refmag' => '$Kmag;',
                          'extinct' => '0.05*($airmass - 1.0);',
                          'zeropt' => 24.0,
+                     },
+                     'K98' => {
+                         'catalogue' => '2mass',
+                         'refmag' => '$Kmag;',
+                         'extinct' => '0.05*($airmass - 1.0);',
+                         'zeropt' => 24.0,
+                     },
+                     'Z' => {
+                         'catalogue' => 'fs',
+                         'refmag' => '$Zmag;',
+                         'extinct' => '0.05*($airmass - 1.0);',
+                         'zeropt' => 24.0,
+                     },
+                     'I' => {
+                         'catalogue' => 'fs',
+                         'refmag' => '$Imag;',
+                         'extinct' => '0.05*($airmass - 1.0);',
+                         'zeropt' => 24.0,
+                     },
+                     'L98' => {
+                         'catalogue' => 'fs',
+                         'refmag' => '$Lmag;',
+                         'extinct' => '0.05*($airmass - 1.0);',
+                         'zeropt' => 24.0,
+                     },
+                     'M98' => {
+                         'catalogue' => 'fs',
+                         'refmag' => '$Mmag;',
+                         'extinct' => '0.05*($airmass - 1.0);',
+                         'zeropt' => 24.0,
                      }
                     );
  
@@ -26,6 +56,13 @@
                        'columns' => ['Jmag','Hmag','Kmag'],
                        'vars' => ['Jmag','Hmag','Kmag'],
                        'vizcat' => '2mass'
-                    }
+                    },
+                   'fs' => {
+                       'location' => "$ENV{ORAC_DATA_CAL}/fs_izjhklm.fit",
+                       'accessmethod' => "searchfits",
+                       'columns' => ['I','Z','J(98)','H(98)','K(98)','L(98)','M(98)'],
+                       'vars' => ['Imag','Zmag','Jmag','Hmag','Kmag','Lmag','Mmag'],
+                       'vizcat' => ''
+                       }
                    );
  
