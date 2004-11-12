6c5c7377c7a703b5050619a67b8402c223185077
Jim Lewis <jrl@ast.cam.ac.uk>
2004-11-12 03:13:36 +0000

    modified findgroup so that group number is never 0
    svn path=/trunk/; revision=5829

--- src/lib/perl5/ORAC/Frame/WFCAM.pm
+++ src/lib/perl5/ORAC/Frame/WFCAM.pm
@@ -601,26 +601,26 @@ sub findgroup {
   if (! $hdrgrp) {
       $hdrgrp = $self->hdr('GRPNUM');
   }
-  my $amiagroup;
+#  my $amiagroup;
   
   # NB: Test for GRPMEM is not 'T' as it used to be.  FITS header reader
   # now silently converts boolean values to 1 or 0.
   
-  if (!defined $self->hdr('GRPMEM')){
-    $amiagroup = 1;
-  } elsif ($self->hdr('GRPMEM') eq "1") {
-    $amiagroup = 1;
-  } else {
-    $amiagroup = 0;
-  }
+#  if (!defined $self->hdr('GRPMEM')){
+#    $amiagroup = 1;
+#  } elsif ($self->hdr('GRPMEM') eq "1") {
+#    $amiagroup = 1;
+#  } else {
+#    $amiagroup = 0;
+#  }
                   
   # Is this group name set to anything useful
  
-  if (!$hdrgrp || !$amiagroup ) {
-    # if the group is invalid there is not a lot we can do
-    # so we just assume 0
-    $hdrgrp = 0;
-  }
+#  if (!$hdrgrp || !$amiagroup ) {
+#    # if the group is invalid there is not a lot we can do
+#    # so we just assume 0
+#    $hdrgrp = 0;
+#  }
  
   $self->group($hdrgrp);
  
