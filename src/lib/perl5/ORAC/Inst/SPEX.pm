116ad9abe2ccee986f051129b5f5b045d9569503
Malcolm J. Currie <mjc@star.rl.ac.uk>
2005-03-25 21:53:35 +0000

    Original version.
    svn path=/trunk/; revision=6059

---
+++ src/lib/perl5/ORAC/Inst/SPEX.pm
@@ -0,0 +1,99 @@
+package ORAC::Inst::SPEX;
+
+=head1 NAME
+
+ORAC::Inst::SPEX - ORAC description of SPEX
+
+=head1 SYNOPSIS
+
+  use ORAC::Inst::SPEX;
+
+  %Mon = $inst->start_algorithm_engines;
+
+=head1 DESCRIPTION
+
+This module configures the system for the instrument.  This primarily
+involves configuring the messaging and algorithm engine environment
+and is independent of the C<ORAC::Frame> definition.
+
+Algorithm engine definitions can be found in C<ORAC::Inst::Defn>.
+
+=cut
+
+use Carp;
+use strict;
+use vars qw/$VERSION/;
+'$Revision$ ' =~ /.*:\s(.*)\s\$/ && ($VERSION = $1);
+
+use base qw/ ORAC::Inst /;
+
+# Status handling
+use ORAC::Constants qw/:status/;
+
+use ORAC::Inst::Defn qw/ orac_determine_initial_algorithm_engines /;
+
+
+=head1 METHODS
+
+=over 4
+
+=item B<start_algorithm_engines>
+
+Starts the algorithm engines and returns a hash containing
+the objects associated with each monolith.
+The routine returns when all the last monolith can be contacted
+(so requires that messaging has been initialised before this
+routine is called).
+
+SPEX uses PHOTOM (photom_mon), CCDPACK (ccdpack_red, ccdpack_res, 
+ccdpack_reg), KAPPA (kappa_mon, ndfpack_mon), POLPACK (polpack_mon),
+CURSA (catselect) and EXTRACTOR (extractor).
+
+=cut
+
+sub start_algorithm_engines {
+  my $self = shift;
+
+  # Retrieve algorithm requirements
+  my @engines = orac_determine_initial_algorithm_engines( 'SPEX' );
+
+  # Now launch them
+  return $self->_launch_algorithm_engines( @engines );
+
+}
+
+=item B<return_possible_calibrations>
+
+Returns an array containing a list of the possible calibrations
+for this instrument.
+
+=cut
+
+sub return_possible_calibrations {
+  my $self = shift;
+  return ( "bias", "dark", "flat", "mask", "sky", "badobs", "readnoise", "referenceoffset" );
+
+}
+
+=back
+
+=head1 SEE ALSO
+
+L<ORAC::Inst::UFTI>
+
+=head1 REVISION
+
+$Id$
+
+=head1 AUTHORS
+
+Malcolm J. Currie E<lt>mjc@jach.hawaii.eduE<gt>.
+
+=head1 COPYRIGHT
+
+Copyright (C) 2003-2005 Particle Physics and Astronomy Research
+Council. All Rights Reserved.
+
+
+=cut
+
