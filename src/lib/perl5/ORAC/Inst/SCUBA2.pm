663481e1f7d46f2bcda5eddfc5d6600c859cedfa
Tim Jenness <t.jenness@jach.hawaii.edu>
2005-03-28 23:15:04 +0000

    Initial version for SCUBA-2. Does not prestart any tasks
    svn path=/trunk/; revision=6069

---
+++ src/lib/perl5/ORAC/Inst/SCUBA2.pm
@@ -0,0 +1,109 @@
+package ORAC::Inst::SCUBA2;
+
+=head1 NAME
+
+ORAC::Inst::SCUBA2 - ORAC description of SCUBA-2
+
+=head1 SYNOPSIS
+
+  use ORAC::Inst::SCUBA2;
+
+  $inst = new ORAC::Inst::SCUBA2;
+
+  %Mon = $inst->start_algorithm_engines;
+
+=head1 DESCRIPTION
+
+This module configures the system for the instrument. This primarily
+involves configuring the messaging and algorithm engine environment
+and is independent of the C<ORAC::Frame> definition.
+
+Algorithm engine definitions can be found in C<ORAC::Inst::Defn>.
+
+=cut
+
+use Carp;
+use strict;
+use vars qw/$VERSION/;
+'$Revision$ ' =~ /.*:\s(.*)\s\$/ && ($VERSION = $1);
+
+use base qw/ ORAC::Inst /;
+
+# Status handling
+use ORAC::Constants qw/:status/;
+use ORAC::Inst::Defn qw/ orac_determine_initial_algorithm_engines /;
+
+=head1 METHODS
+
+=over 4
+
+=item B<start_algorithm_engines>
+
+Starts the algorithm engines and returns a hash containing the objects
+associated with each monolith.  The routine returns when all the last
+monolith can be contacted (so requires that messaging has been
+initialised before this routine is called).
+
+SCUBA-2 uses KAPPA (kapview_mon, kappa_mon, ndfpack_mon) and
+CCDPACK.
+
+=cut
+
+sub start_algorithm_engines {
+  my $self = shift;
+
+  # Retrieve algorithm requirements
+  my @engines = orac_determine_initial_algorithm_engines( 'SCUBA2' );
+
+  # Now launch them
+  return $self->_launch_algorithm_engines( @engines );
+
+}
+
+=item B<return_possible_calibrations>
+
+Returns an array containing a list of the possible calibrations
+for this instrument.
+
+=cut
+
+sub return_possible_calibrations {
+  my $self = shift;
+  return ( "gains", "tausys" );
+}
+
+=back
+
+=head1 SEE ALSO
+
+L<ORAC::Inst::SCUBA>
+
+=head1 REVISION
+
+$Id$
+
+=head1 AUTHORS
+
+Tim Jenness E<lt>t.jenness@jach.hawaii.eduE<gt>,
+Alasdair Allan E<lt>aa@astro.ex.ac.ukE<gt>
+
+=head1 COPYRIGHT
+
+Copyright (C) 1998-2005 Particle Physics and Astronomy Research
+Council. All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful,but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place,Suite 330, Boston, MA  02111-1307, USA
+
+=cut
+
