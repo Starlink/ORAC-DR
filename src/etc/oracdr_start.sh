aba7df3aeb0500dcb1accafa7e0e5145059ca7f4
Brad Cavanagh <b.cavanagh@jach.hawaii.edu>
2006-09-06 23:45:33 +0000

    fix for proper bash scripting
    svn path=/trunk/; revision=6621

--- src/etc/oracdr_start.sh
+++ src/etc/oracdr_start.sh
@@ -37,30 +37,11 @@
 
 #  History:
 #     $Log$
+#     Revision 1.2  2006/09/06 23:45:33  bradc
+#     fix for proper bash scripting
+#
 #     Revision 1.1  2006/09/06 02:30:11  bradc
 #     initial addition
-#
-#     Revision 1.7  2002/04/04 08:21:20  mjc
-#     Allow for multiple Starlink User Notes.
-#
-#     Revision 1.6  2001/03/19 23:33:30  timj
-#     Add oracdr_monitor
-#
-#     Revision 1.5  2000/10/11 01:13:51  timj
-#     Add oracdr_parse_recipe
-#
-#     Revision 1.4  2000/02/03 04:52:50  timj
-#     Slight change to startup screen
-#
-#     Revision 1.3  2000/02/03 03:44:09  timj
-#     Add check for existence of IN/OUT directories
-#
-#     Revision 1.2  2000/02/03 03:14:18  timj
-#     Add ORACDR_VERSION
-#
-#     Revision 1.1  2000/02/03 02:50:45  timj
-#     Starlink startup scripts
-#
 
 #  Revision:
 #     $Id$
@@ -83,51 +64,51 @@
 # and allow that to be used in preference to the starlink version
 # if set (and if it exists)
 
-if ( "$ORAC_PERLBIN" != "" ); then
-  set starperl = $ORAC_PERLBIN
-elif ( -e STAR_PERL ); then
-  set starperl = STAR_PERL
+if test ! -z "${ORAC_PERLBIN}"; then
+  starperl=$ORAC_PERLBIN
+elif test -e STAR_PERL; then
+  starperl=STAR_PERL
 else
-  set starperl = NONE
+  starperl=NONE
 fi
 
 # Set up back door for the version number
 
-if ( "$ORACDR_VERSION" != "" ); then
-  set pkgvers = $ORACDR_VERSION
+if test -z "${ORACDR_VERSION}"; then
+  pkgvers=$ORACDR_VERSION
 else
-  set pkgvers = PKG_VERS
+  pkgvers=PKG_VERS
 fi
 
 
 # These are perl programs
 
-if ( -e $starperl ); then
+if test -e $starperl; then
 
   # ORAC-DR
   # Might have an argument to oracdr passed in to this routine.
   # Therefore need to check for $oracdr_args shell variable
   # and use it for the alias to oracdr
-  if !("$oracdr_args" != ""); then
-    set oracdr_args ''
+  if ! test -z "${oracdr_args}"; then
+    oracdr_args=''
   fi
 
-oracdr () {      "$starperl  ${ORAC_DIR}/bin/oracdr $oracdr_args" ${1+"$@"}; }
-oracdr_db () {   "$starperl -d ${ORAC_DIR}/bin/oracdr" ${1+"$@"}; }
-oracdr_nuke () { "$starperl  ${ORAC_DIR}/bin/oracdr_nuke" ${1+"$@"}; }
-oracdisp () {    "$starperl  ${ORAC_DIR}/bin/oracdisp" ${1+"$@"}; }
-oracdr_parse_recipe () { "$starperl ${ORAC_DIR}/bin/oracdr_parse_recipe" ${1+"$@"}; }
-oracdr_monitor () { "$starperl ${ORAC_DIR}/bin/oracdr_monitor" ${1+"$@"}; }
+alias oracdr="$starperl ${ORAC_DIR}/bin/oracdr ${oracdr_args}"
+alias oracdr_db="$starperl -d ${ORAC_DIR}/bin/oracdr"
+alias oracdr_nuke="$starperl ${ORAC_DIR}/bin/oracdr_nuke"
+alias oracdisp="$starperl ${ORAC_DIR}/bin/oracdisp"
+alias oracdr_parse_recipe="$starperl ${ORAC_DIR}/bin/oracdr_parse_recipe"
+alias oracdr_monitor="$starperl ${ORAC_DIR}/bin/oracdr_monitor"
 
 else
   echo "************ Starlink perl could not be located. ********"
   echo "************       Please install STARPERL       ********"
 
-oracdr () {       echo 'Command not available - needs Starlink PERL' ${1+"$@"}; }
-oracdr_db () {    echo 'Command not available - needs Starlink PERL' ${1+"$@"}; }
-oracdr_nuke () {  echo 'Command not available - needs Starlink PERL' ${1+"$@"}; }
-oracdisp () {     echo 'Command not available - needs Starlink PERL' ${1+"$@"}; }
-oracdr_monitor () { echo 'Command not available - needs Starlink PERL' ${1+"$@"}; }
+alias oracdr="echo 'Command not available - needs Starlink PERL'"
+alias oracdr_db="echo 'Command not available - needs Starlink PERL'"
+alias oracdr_nuke="echo 'Command not available - needs Starlink PERL'"
+alias oracdisp="echo 'Command not available - needs Starlink PERL'"
+alias oracdr_monitor="echo 'Command not available - needs Starlink PERL'"
 
 fi
 
@@ -136,22 +117,26 @@ fi
 oracman () {     'csh ${ORAC_DIR}/bin/oracman' ${1+"$@"}; }
 
 # Define default documentation instruction.
-set doc_command = "'showme sun${ORAC_SUN}'"
+doc_command="'showme sun${ORAC_SUN}'"
 
 # Allow for more than one document per instrument.  Determine whether
 # or not there is a comma in document number.
-set comma_index = `echo ${ORAC_SUN} | awk '{print index($0,",")}'`
+set comma_index=`echo ${ORAC_SUN} | awk '{print index($0,",")}'`
 if ( $comma_index > 0 ); then
 
 # Extract the document numbers.
-   set doc_numbers = `echo ${ORAC_SUN} | awk '{i=1;while(i<=split($0,a,",")){print a[i];i++}}'`
+   doc_numbers=`echo ${ORAC_SUN} | sed -e 's/,/ /g'`
+   doc_num_array=($doc_numbers)
 
 # Form concatenated instruction giving options for finding documentation. 
-   set doc_command = "'showme sun$doc_numbers[1]'"
-   shift doc_numbers
-   foreach doc ( $doc_numbers )
-      set doc_command = "$doc_command or 'showme sun$doc'"
-   end
+   doc_command="'showme sun${doc_num_array[0]}'"
+   element_count=${#doc_num_array[@]}
+   index=1
+   while [ "$index" -lt "$element_count" ]
+   do
+      doc_command="${doc_command} or 'showme sun${doc_num_array[$index]}'"
+      let "index = $index + 1"
+   done
 fi
 
 # Start up message
@@ -166,15 +151,15 @@ echo " "
 echo " Raw data will be read from $ORAC_DATA_IN"
 
 # Check for that `in' directory
-if !( -d $ORAC_DATA_IN ); then
-  echo '     \!\!\!\!\!\!\!\!\!\!\!\! but that directory does not exist \!\!\!\!\!\!\!\!\! '
+if ! test -d $ORAC_DATA_IN; then
+  echo '     !!!!!!!!!!!! but that directory does not exist !!!!!!!!! '
 fi
 
 echo " Reduced data will appear in $ORAC_DATA_OUT"
 
 # Check for that `out' directory
-if !(-d $ORAC_DATA_OUT); then
-  echo '     \!\!\!\!\!\!\!\!\!\!\!\! but that directory does not exist \!\!\!\!\!\!\!\!\! '
+if ! test -d $ORAC_DATA_OUT; then
+  echo '     !!!!!!!!!!!! but that directory does not exist !!!!!!!!! '
 fi
 
 echo " "
