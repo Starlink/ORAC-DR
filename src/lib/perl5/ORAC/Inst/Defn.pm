858d3976c0e1fc8e1a1d79d38389692cb8d887e2
Tim Jenness <t.jenness@jach.hawaii.edu>
2011-02-15 13:04:13 -1000

    lib: FTS2DR is no longer a DRAMA task
    It's just part of SMURF.

--- src/lib/perl5/ORAC/Inst/Defn.pm
+++ src/lib/perl5/ORAC/Inst/Defn.pm
@@ -265,11 +265,6 @@ my %MonolithDefns = (
            CLASS => 'ORAC::Msg::Task::ADAM',
            PATH => \&fluxes_helper,
           },
-         FTS2DR => {
-           MESSYS => 'DRAMA',
-           CLASS => 'ORAC::Msg::Task::DRAMA',
-           PATH => (exists $ENV{FTS2DR_DIR} ? $ENV{FTS2DR_DIR}. "/fts2dr.sh" :
-		                                 "not_defined"),
          },
          hdstools_mon => {
                           MESSYS => 'AMS',
