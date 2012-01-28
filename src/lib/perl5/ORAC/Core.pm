Starlink/ORAC-DR

commit c7802327db158b555809fa0cedb46d38eafa4dcd

fix up combined batch/flag operation so that things like "-from 1 -loop flag -batch" use the flag files for data detection
svn path=/trunk/; revision=8039

src/lib/perl5/ORAC/Core.pm
@@ -1055,9 +1055,15 @@ sub orac_process_argument_list {
 
         # Special case if we are doing data detection since the
         # final obs number is changing
-        if (!defined $opt{loop} or ($opt{loop} ne 'wait' and $opt{loop} ne 'flag')) {
+        if ( ! defined $opt{loop} or
+             ( $opt{loop} ne 'wait' and $opt{loop} ne 'flag' ) or
+             ( $opt{loop} eq 'flag' and defined( $opt{batch} ) ) ) {
 
-          my ($next, $high) = orac_check_data_dir($frameclass, $opt{from}, 0);
+          my $flag = ( defined( $opt{loop} ) and
+                       $opt{loop} eq 'flag'  and
+                       defined( $opt{batch} ) );
+
+          my ($next, $high) = orac_check_data_dir($frameclass, $opt{from}, $flag);
 
           if (defined $high) {
 
@@ -1075,7 +1081,7 @@ sub orac_process_argument_list {
 
         } else {
 
-          # If we are using -loop wait and flag we can not optimize 
+          # If we are using -loop wait and flag we can not optimize
           # to -list since the file count is changing
           @$obs = ( $opt{from} );
           $loop = $opt{loop};

commit 85cf92fda48fcd28e28aca25a43ec63aec3ca6d4

lib: Fix -nobatch -loop flag
Commit c7802327db158b555809fa0cedb46d38eafa4dcd broke it when adding
support for -batch -loop flag. Defined-ness is not the same as truth.

src/lib/perl5/ORAC/Core.pm
@@ -1220,11 +1220,11 @@ sub orac_process_argument_list {
         # final obs number is changing
         if ( ! defined $opt{loop} or
              ( $opt{loop} ne 'wait' and $opt{loop} ne 'flag' ) or
-             ( $opt{loop} eq 'flag' and defined( $opt{batch} ) ) ) {
+             ( $opt{loop} eq 'flag' and $opt{batch} ) ) {
 
           my $flag = ( defined( $opt{loop} ) and
                        $opt{loop} eq 'flag'  and
-                       defined( $opt{batch} ) );
+                       $opt{batch} );
 
           my ($next, $high) = orac_check_data_dir($frameclass, $opt{from}, $flag);
 