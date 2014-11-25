148df69f4bfac8e8a9d1aba988c7642e993c25fa
Malcolm J. Currie <mjc@star.rl.ac.uk>
2014-11-24 22:35:52 -1000

    Calib: Switch off warnings for JCMT qaparams.

--- src/lib/perl5/ORAC/Calib/JCMT.pm
+++ src/lib/perl5/ORAC/Calib/JCMT.pm
@@ -73,7 +73,7 @@ Return or set the filename for QA parameters.
 
 sub qaparams {
   my $self = shift;
-  my $qaparamsfile = $self->GenericIndexAccessor( "qaparams", 0, 0, 1, 1, @_ );
+  my $qaparamsfile = $self->GenericIndexAccessor( "qaparams", 0, 0, 1, 0, @_ );
   # Find this file on disk because it will not be in ORAC_DATA_OUT
   return $self->find_file( $qaparamsfile );
 }
